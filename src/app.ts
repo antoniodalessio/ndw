f7c1c10e39ee5248f342056b68cc35e9025f65cb
Antonio D'Alessio <antonio@adias.it>
2019-12-10 10:44:58 +0100

    init mongo db to store deployements data

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 var express = require('express')
 var shell = require('shelljs')
+const MongoClient = require('mongodb').MongoClient;
 
 export default class App {
 
@@ -15,6 +16,32 @@ export default class App {
     this.expressApp.post('/deploy/:project', function (req, res) {
       shell.exec(`cd ${process.env.PROJECSTPATH}${req.params.project} && git pull && npm run tsc && pm2 restart 0`)
       res.status(200).json({ msg: 'ok' });
+    });
+
+    this.initMongoDB()
+  }
+
+
+  initMongoDB() {
+
+    // Connection URL
+    const url = 'mongodb://localhost:27017';
+
+    const dbName = 'ndw';
+
+    MongoClient.connect(url, function(err, client) {
+      
+      console.log("Connected successfully to server");
+
+      const db = client.db(dbName);
+
+      const collection = db.collection('deployments');
+
+      collection.insertMany([
+        {a : 1}, {a : 2}, {a : 3}
+      ])
+     
+      client.close();
     });
   }
 
