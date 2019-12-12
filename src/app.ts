3852f2b461fde3412f274168d3b6f2fa8c3e5367
Antonio D'Alessio <antonio@adias.it>
2019-12-12 19:07:44 +0100

    ...

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 var express = require('express')
-var shell = require('shelljs')
+var shell = require('async-shelljs')
 const nodemailer = require('nodemailer');
 const MongoClient = require('mongodb').MongoClient;
 
@@ -12,21 +12,51 @@ export default class App {
   constructor(config) {
     //this.initMongoDB()
     this.initMailer()
-    this.initServer()      
+    this.initServer()   
   }
 
-  deploy(req, res) {
+  async deploy(req, res) {
     try {
-      shell.exec(`cd ${process.env.PROJECSTPATH}${req.params.project} && git pull && npm run tsc && pm2 restart 0`)
-      res.status(200).json({ msg: 'ok' });
-      this.sendEmail(`${req.params.project} deploy success`, `${req.params.project} deploy success`)
-      /*if (.code != 0) {
-        //this.store(req.params.project);
-        res.status(200).json({ msg: 'ok' });
-        this.sendEmail(`${req.params.project} deploy success`, `${req.params.project} deploy success`)
-      }else{
+
+      let commands = [
+        {
+          cmd: 'cd',
+          args: `${process.env.PROJECSTPATH}${req.params.project}`
+        },
+        {
+          cmd: 'git',
+          args: `pull`
+        },
+        {
+          cmd: 'npm',
+          args: `run tsc`
+        },
+        {
+          cmd: 'pm2',
+          args: `restart 0`
+        }
+      ]
+
+      let result = await commands.reduce( async (previousPromise, next) => {
+        let prevresult:any = await previousPromise;
+        let cmd = next.cmd + ' ' + next.args
+        let commandExists = await shell.which(next.cmd)
+        if (prevresult.code == 0 && commandExists) {
+          return shell.exec(cmd)
+        }else{
+          return Promise.resolve({code: 1})
+        }
+      }, Promise.resolve({code: 0}))
+
+      console.log("result", result)
+
+      if (result.code == 1) {
         this.sendEmail(`${req.params.project} deploy failed`, "")
-      }*/
+        //shell.exit(0)
+      }else{
+        this.sendEmail(`${req.params.project} deploy success`, `${req.params.project} deploy success`)
+      }      
+
     } catch (e) {
       this.sendEmail(`${req.params.project} deploy failed`, JSON.stringify(e))
     }
@@ -56,14 +86,13 @@ export default class App {
 
   }
 
-
   initServer() {
     this.expressApp = express();
     this.expressApp.listen(3010, () => {
       console.log('Server running on port 3010!');
     });
 
-    this.expressApp.post('/deploy/:project', (req, res) => this.deploy(req, res))
+    this.expressApp.post('/deploy/:project', async (req, res) => await this.deploy(req, res))
   }
 
   initMailer() {
